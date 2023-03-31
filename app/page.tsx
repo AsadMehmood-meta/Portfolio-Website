9b49524b4d8b6cbcee01a566ad5b344bbfa84384
Asad Mehmood <asadmehmood96@gmail.com>
2023-03-31 22:03:57 +0500

    set scroll top button

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-
+"use client"
+import React, { useEffect, useState } from 'react'
 import { About } from "./components/About"
 import { Contact } from "./components/Contact"
 import { Main } from "./components/Main"
@@ -7,6 +8,22 @@ import { Skills } from "./components/Skills"
 import { HiOutlineChevronDoubleUp } from 'react-icons/hi'
 
 export default function Home() {
+
+  const [scroll, setScroll] = useState(false)
+
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY >= 90) {
+        setScroll(true)
+      }
+      else {
+        setScroll(false)
+      }
+    }
+    window.addEventListener("scroll", handleScroll)
+  }, [])
+
   return (
     <div>
       <Main />
@@ -14,12 +31,16 @@ export default function Home() {
       <Skills />
       <Projects />
       <Contact />
-      <a className='fixed bottom-5 right-5 lg:fixed lg:bottom-10 lg:right-10 rounded-full shadow-xl shadow-gray-400 cursor-pointer p-2 bg-stone-100 hover:scale-125 ease-in duration-100'
-        href="#">
-        <HiOutlineChevronDoubleUp
-          className='text-[#a0522d]'
-          size={30} />
-      </a>
+      <div className={scroll ? 'fixed bottom-5 right-5 lg:fixed lg:bottom-10 lg:right-10 rounded-full shadow-xl shadow-gray-400 cursor-pointer p-2 bg-stone-100 hover:scale-125 ease-in duration-100'
+        : "hidden"
+      }>
+        <a className=''
+          href="#">
+          <HiOutlineChevronDoubleUp
+            className='text-[#a0522d]'
+            size={30} />
+        </a>
+      </div>
     </div>
   )
 }
